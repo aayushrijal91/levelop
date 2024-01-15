cb9d138d307c5367c9d4831ebdf5d4a752bc00b3
Shanti-PL <fanjunyi1998@163.com>
2024-01-15 15:00:00 +1100

    form updated

--- app/components/form.php
+++ app/components/form.php
@@ -2,6 +2,14 @@
 
 include __DIR__ .'/../function.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../vendor/phpmailer/phpmailer/src/SMTP.php';
+require '../vendor/autoload.php';
+
  if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token'])) {
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = $recaptcha_server_secret;
@@ -26,12 +34,12 @@ include __DIR__ .'/../function.php';
         $room_type = $_POST['room-type'];
         $notes = $_POST['message'];
 
-        $service = '';
-        if (!empty($serviceTop)) {
-            $service = $serviceTop;
-        } else if (!empty($serviceFooter)) {
-            $service = $serviceFooter;
-        }
+        // $service = '';
+        // if (!empty($serviceTop)) {
+        //     $service = $serviceTop;
+        // } else if (!empty($serviceFooter)) {
+        //     $service = $serviceFooter;
+        // }
 
         $message = '<!DOCTYPE html>
                 <html>
@@ -85,23 +93,52 @@ include __DIR__ .'/../function.php';
             '<td><b>' . strip_tags($notes) . '</b></td>' .
             '</tr>' . 
             '</tbody></table></body></html>';
+        
+        _phpmailer($to, $site, $subject, $message, $no_reply_email);
 
-        $headers = "MIME-Version: 1.0\r\n" .
-            "Content-type: text/html; charset=utf-8\r\n" .
-            "From: " . $site . " <" . $no_reply_email . ">" . "\r\n" .
-             "Bcc: " . $bcc_email . "\r\n" .
-            "Reply-To: " . $site . " <" . $no_reply_email . ">" . "\r\n" .
-            "X-Mailer: PHP/" . phpversion();
-        $result = mail($to, $subject, $message, $headers);
+        header('location:./../thankyou.php');
 
-        if ($result) {
-            header('location:./../thankyou.php');
-        } else {
-            throw new Exception('Failed, please submit form again or call us directly.');
-        }
     } catch (Exception $e) {
         echo '<script language="javascript">alert("' . $e->getMessage() . '")</script>';
         echo '<script language="javascript">history.go(-1);</script>';
     }
 }
  
+function _phpmailer($to_email, $site, $subject, $message, $no_reply_email, $cc = "", $bcc = "")
+{
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = TRUE;
+        $mail->SMTPSecure = "ssl";
+        $mail->Port     = 465;
+        $mail->Username = "jfan@aiims.com.au";
+        $mail->Password = "phow ajje izkd otte";
+        $mail->Host     = "smtp.gmail.com";
+        $mail->Mailer   = "smtp";
+        $mail->SetFrom($no_reply_email, $site);
+        $mail->AddAddress($to_email);
+        if ($cc != '') {
+            $mail->addCC($cc);
+        }
+        if ($bcc != '') {
+            $mail->addBCC($bcc);
+        }
+        $mail->Subject = $subject;
+        $mail->WordWrap   = 80;
+        $mail->MsgHTML($message);
+
+        if ((isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)) {
+            $file_name = $_FILES['uploadedFile']['name'];
+            $file_path = $_FILES['uploadedFile']['tmp_name'];
+            $mail->addAttachment($file_path, $file_name);
+        }
+
+        $mail->IsHTML(true);
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
